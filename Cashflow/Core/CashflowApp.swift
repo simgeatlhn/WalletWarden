5f45888e50c2f486adc37919af3f49da49f0233d
simgeatlhn <simgeeatlihan@gmail.com>
2023-04-17 11:04:02 +0300

    initial commit

---
+++ Cashflow/Core/CashflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashflowApp.swift
+//  Cashflow
+//
+//  Created by simge on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CashflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
