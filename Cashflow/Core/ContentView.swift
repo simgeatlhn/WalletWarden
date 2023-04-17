6d3ec525e12b976d54b4341ae9ce3458301de986
simgeatlhn <simgeeatlihan@gmail.com>
2023-04-17 11:44:44 +0300

    Added TabBar and NavigationBar

--- Cashflow/Core/ContentView.swift
+++ Cashflow/Core/ContentView.swift
@@ -8,16 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    var userName = "Hello, Simge"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            TabView {
+                Text("Home")
+                    .tabItem {
+                        Image(systemName: "homekit")
+                        Text("Home")
+                    }
+                Text("Wallet")
+                    .tabItem {
+                        Image(systemName: "folder")
+                        Text("Profile")
+                    }
+                Text("Profile")
+                    .tabItem {
+                        Image(systemName: "person")
+                        Text("Profile")
+                    }
+            }
+            .navigationBarTitle(Text(userName), displayMode: .large)
+            .navigationBarItems(trailing:
+                                    HStack {
+                Image(systemName: "bell")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .frame(width: 32, height: 32)
+            }
+            )
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
