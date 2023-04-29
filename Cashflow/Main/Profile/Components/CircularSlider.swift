c80ebc87ac320d88bb43c097fae16eb10f27cedf
simgeatlhn <simgeeatlihan@gmail.com>
2023-04-29 13:12:50 +0300

    Fix: Prevented manual user interaction on circular slider

--- Cashflow/Main/Profile/Components/CircularSlider.swift
+++ Cashflow/Main/Profile/Components/CircularSlider.swift
@@ -44,6 +44,7 @@ struct CircularSlider: View {
 struct ThumbSlider: View {
     @Binding var value: Double
     var percentage: Double
+    @State private var isUserInteractionEnabled = false
     
     var body: some View {
         ZStack {
@@ -54,13 +55,15 @@ struct ThumbSlider: View {
         }
         .gesture(DragGesture(minimumDistance: 0)
             .onChanged { value in
-                let vector = CGVector(dx: value.location.x, dy: value.location.y)
-                let radians = atan2(vector.dy - 100, vector.dx - 100)
-                var angle = radians * 180 / .pi
-                if angle < 0 {
-                    angle += 360
+                if isUserInteractionEnabled {
+                    let vector = CGVector(dx: value.location.x, dy: value.location.y)
+                    let radians = atan2(vector.dy - 100, vector.dx - 100)
+                    var angle = radians * 180 / .pi
+                    if angle < 0 {
+                        angle += 360
+                    }
+                    self.value = Double(angle / 360)
                 }
-                self.value = Double(angle / 360)
             }
         )
         .onAppear {
