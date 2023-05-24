0885175459ef937b9bbe1502350e6126ea979b1a
simgeatlhn <simgeeatlihan@gmail.com>
2023-05-24 14:12:34 +0300

    Added navigation functionality to the buttons in the ActionButtonsView

--- Cashflow/Main/ActionButtons/ActionButtonsView.swift
+++ Cashflow/Main/ActionButtons/ActionButtonsView.swift
@@ -8,53 +8,62 @@
 import SwiftUI
 
 struct ActionButtonsView: View {
+    @State private var newBalance: String = "$0"
+    @StateObject private var walletViewModel = WalletViewModel()
+    
     var body: some View {
         HStack {
             Spacer()
             
-            VStack(alignment: .center) {
-                Image(systemName: "plus")
-                    .font(.system(size: 32))
-                    .foregroundColor(.black)
-                Spacer().frame(height: 11)
-                Text("Add Money")
-                    .font(.system(size: 16))
-                    .foregroundColor(blackColor)
+            NavigationLink(destination: BalanceInputView(newBalance: $newBalance, walletViewModel: walletViewModel)) {
+                VStack(alignment: .center) {
+                    Image(systemName: "plus")
+                        .font(.system(size: 32))
+                        .foregroundColor(.black)
+                    Spacer().frame(height: 11)
+                    Text("Add Money")
+                        .font(.system(size: 16))
+                        .foregroundColor(blackColor)
+                }
+                .frame(width: 110, height: 110)
+                .background(purpleColor)
+                .cornerRadius(16)
             }
-            .frame(width: 110, height: 110)
-            .background(purpleColor)
-            .cornerRadius(16)
             
             Spacer()
             
-            VStack(alignment: .center) {
-                Image(systemName: "arrow.left.arrow.right")
-                    .font(.system(size: 32))
-                    .foregroundColor(.black)
-                Spacer()
-                    .frame(height: 4)
-                Text("Expenses")
-                    .font(.system(size: 16))
-                    .foregroundColor(blackColor)
+            NavigationLink(destination: WalletView().environmentObject(walletViewModel)) {
+                VStack(alignment: .center) {
+                    Image(systemName: "arrow.left.arrow.right")
+                        .font(.system(size: 32))
+                        .foregroundColor(.black)
+                    Spacer()
+                        .frame(height: 4)
+                    Text("Expenses")
+                        .font(.system(size: 16))
+                        .foregroundColor(blackColor)
+                }
+                .frame(width: 110, height: 110)
+                .background(orangeColor)
+                .cornerRadius(16)
             }
-            .frame(width: 110, height: 110)
-            .background(orangeColor)
-            .cornerRadius(16)
             
             Spacer()
             
-            VStack(alignment: .center) {
-                Image(systemName: "square.and.arrow.down")
-                    .font(.system(size: 32))
-                    .foregroundColor(blackColor)
-                Spacer().frame(height: 4)
-                Text("Categories")
-                    .font(.system(size: 16))
-                    .foregroundColor(blackColor)
+            NavigationLink(destination: ProfileView().environmentObject(walletViewModel)) {
+                VStack(alignment: .center) {
+                    Image(systemName: "square.and.arrow.down")
+                        .font(.system(size: 32))
+                        .foregroundColor(blackColor)
+                    Spacer().frame(height: 4)
+                    Text("Categories")
+                        .font(.system(size: 16))
+                        .foregroundColor(blackColor)
+                }
+                .frame(width: 108, height: 108)
+                .background(blueColor)
+                .cornerRadius(16)
             }
-            .frame(width: 108, height: 108)
-            .background(blueColor)
-            .cornerRadius(16)
             
             Spacer()
         }
@@ -62,11 +71,10 @@ struct ActionButtonsView: View {
     }
 }
 
-
-
-
 struct ActionButtonsView_Previews: PreviewProvider {
     static var previews: some View {
-        ActionButtonsView()
+        NavigationView {
+            ActionButtonsView()
+        }
     }
 }
